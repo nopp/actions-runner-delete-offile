6d700e7a78eac3aca2c961966f4e9d9924528bfd
Carlos Augusto Malucelli <camalucelli@gmail.com>
2023-07-10 21:48:21 -0300

    Update delete-runners.py

--- delete-runners.py
+++ delete-runners.py
@@ -4,11 +4,15 @@ import json
 RUNNER_TOKEN="xxxxx"
 ORG_NAME="yyyy"
 
-headers = {"Accept":"application/vnd.github+json","Authorization":"Bearer "+RUNNER_TOKEN,"X-GitHub-Api-Version":"2022-11-28"}
+headers = { "Accept":"application/vnd.github+json",
+            "Authorization":"Bearer "+RUNNER_TOKEN,
+            "X-GitHub-Api-Version":"2022-11-28"
+           }
 
 r = requests.get('https://api.github.com/orgs/'+ORG_NAME+'/actions/runners',headers=headers).json()['runners']
 for runner in r:
     if runner['status'] == "offline":
         print("Removing "+str(runner['id']))
         d = requests.delete('https://api.github.com/orgs/'+ORG_NAME+'/actions/runners/'+str(runner['id']),headers=headers)
-        print(d)
+        if str(d.status_code) == "204":
+            print("Runner successfully deleted.")
