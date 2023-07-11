f8f2c910147f0fe82d038a8ab0c57ca7bc9952fc
Carlos Augusto Malucelli <camalucelli@gmail.com>
2023-07-10 21:36:31 -0300

    Create delete-runners.py

---
+++ delete-runners.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+RUNNER_TOKEN="xxxxx"
+ORG_NAME="yyyy"
+
+headers = {"Accept":"application/vnd.github+json","Authorization":"Bearer "+RUNNER_TOKEN,"X-GitHub-Api-Version":"2022-11-28"}
+
+r = requests.get('https://api.github.com/orgs/'+ORG_NAME+'/actions/runners',headers=headers).json()['runners']
+for runner in r:
+    if runner['status'] == "offline":
+        print("Removing "+str(runner['id']))
+        d = requests.delete('https://api.github.com/orgs/'+ORG_NAME+'/actions/runners/'+str(runner['id']),headers=headers)
+        print(d)
